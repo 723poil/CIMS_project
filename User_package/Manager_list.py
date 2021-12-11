8c87316b8d9492c77b85c7c06e7b124a9e50e76e
giunsang <33440755+giunsang@users.noreply.github.com>
2021-12-11 15:42:39 +0900

    list
    list

--- User_package/Manager_list.py
+++ User_package/Manager_list.py
@@ -1,12 +1,18 @@
+import Manager
+
 class Manager_list:
-    def __init__(self, Manager):
-        self.Manager = Manager #회원가입한 User클래스 생성 -> 생성된 클래스는 바로 list 클래스의 배열로 추가된다
+    def __init__(self):
+        self.manager_lsit = [] #회원가입한 Manager클래스 생성 -> 생성된 클래스는 바로 list 클래스의 배열로 추가된다
 
     def Check_Member_id(email, passord):
-        for(i = 0; i < len(Manager) ; i = i + 1)
-        {
-         if Manager[i] != None and email == Manager[i].email and password == Manager[i].password:
-            return 2 # 회원 로그인
-         else
-            return -1
-        }
+        for i in range(0, len(manager_list)):
+            if manager_lsit[i] != None and email == manager_lsit[i].email and password == manager_lsit[i].password:
+                return 2 # 회원 로그인
+            else
+                return -1
+
+    def insert_manager(Manager):
+        manager_list.append(Manager)
+
+        
+        
--- User_package/Member_list.py
+++ User_package/Member_list.py
@@ -1,12 +1,18 @@
+import Memeber
+
 class Member_list:
-    def __init__(self, Member):
-        self.Member = Member #회원가입한 User클래스 생성 -> 생성된 클래스는 바로 list 클래스의 배열로 추가된다
+    def __init__(self):
+        self.member_list = []
+         #회원가입한 Member클래스 생성 -> 생성된 클래스는 바로 list 클래스의 배열로 추가된다
 
     def Check_Member_id(email, passord):
-        for(i = 0; i < len(Member) ; i = i + 1)
-        {
-         if Member[i] != None and email == Member[i].email and password == Member[i].password:
-            return 2 # 회원 로그인
-         else
-            return -1
-        }
+        for i in range(0, len(member_list)):
+            if self.member_list[i] != None and email == self.member_list[i].email and password == self.member_list[i].password:
+                return 2 # 회원 로그인
+            else
+                return -1
+        
+        
+
+    def insert_Member(Member):
+        self.member_list.append(Member)
