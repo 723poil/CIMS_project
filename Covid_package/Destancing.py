3dbe56f3b93170369992f7e4bce7c5af6c67bdd8
giunsang <33440755+giunsang@users.noreply.github.com>
2021-12-11 17:37:35 +0900

    거리두기-접종횟수
    거리두기-접종횟수

--- Covid_package/Destancing.py
+++ Covid_package/Destancing.py
@@ -3,7 +3,7 @@ class Destancing:
     def __init__(self, Destancig):
         self.state = Destancig["state"]
         self.restrictions = Destancig["restrictions"] #형식 변환 주의
+        
 
-
-    def set():
-        pass
+    def check_vaccination_num(vaccination_content): #vaccination_content의 배열 크기 = 접종횟수
+        return len(vaccination_content)
