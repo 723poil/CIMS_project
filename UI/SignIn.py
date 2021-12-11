a0cef1d36d5b91e4bb257eb92ff63e379afb1870
Arecanon <arecanon@gmail.com>
2021-12-11 13:04:22 +0900

    fix UI test code

--- UI/SignIn.py
+++ UI/SignIn.py
@@ -4,11 +4,11 @@ from PyQt5 import uic
 
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
-form_class = uic.loadUiType("StartUI.ui")[0]
-form_error = uic.loadUiType("LoginError.ui")[0]
+form_class = uic.loadUiType("UI\SignIn.ui")[0]
+form_error = uic.loadUiType("UI\LoginError.ui")[0]
 
 #화면을 띄우는데 사용되는 Class 선언
-class WindowClass(QDialog, form_class) :
+class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
