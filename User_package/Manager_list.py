b8d1219886923c2fb32239e4248f892c17f848ef
giunsang <33440755+giunsang@users.noreply.github.com>
2021-12-11 15:45:08 +0900

    list
    list

--- User_package/Manager_list.py
+++ User_package/Manager_list.py
@@ -5,14 +5,14 @@ class Manager_list:
         self.manager_lsit = [] #회원가입한 Manager클래스 생성 -> 생성된 클래스는 바로 list 클래스의 배열로 추가된다
 
     def Check_Member_id(email, passord):
-        for i in range(0, len(manager_list)):
-            if manager_lsit[i] != None and email == manager_lsit[i].email and password == manager_lsit[i].password:
+        for i in range(0, len(self.manager_list)):
+            if self.manager_lsit[i] != None and email == self.manager_lsit[i].email and password == self.manager_lsit[i].password:
                 return 2 # 회원 로그인
             else
                 return -1
 
     def insert_manager(Manager):
-        manager_list.append(Manager)
+        self.manager_list.append(Manager)
 
         
         
