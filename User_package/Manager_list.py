b645bd0ed996c5e7198780d8ba8737240d3a7dcb
723poil <75459370+723poil@users.noreply.github.com>
2021-12-11 17:18:56 +0900

    문법 수정

--- User_package/Manager_list.py
+++ User_package/Manager_list.py
@@ -3,13 +3,14 @@ class Manager_list:
     def __init__(self):
         self.manager_lsit = [] #회원가입한 Manager클래스 생성 -> 생성된 클래스는 바로 list 클래스의 배열로 추가된다
 
-    def Check_Member_id(email, passord):
+    def Check_Manager_id(self, email, password):
         for i in range(0, len(self.manager_list)):
             if self.manager_lsit[i] != None and email == self.manager_lsit[i].email and password == self.manager_lsit[i].password:
-                return 2 # 회원 로그인
-            else
+                return 2 # 관리자 로그인
+        
+        return -1 # 관리자가 아닌 경우 회원 로그인으로 시도
 
-    def insert_manager(Manager):
+    def insert_manager(self, Manager):
         self.manager_list.append(Manager)
 
         
--- User_package/Member_list.py
+++ User_package/Member_list.py
@@ -4,14 +4,14 @@ class Member_list:
         self.member_list = []
          #회원가입한 Member클래스 생성 -> 생성된 클래스는 바로 list 클래스의 배열로 추가된다
 
-    def Check_Member_id(email, passord):
-        for i in range(0, len(member_list)):
+    def Check_Member_id(self, email, password):
+        for i in range(0, len(self.member_list)):
             if self.member_list[i] != None and email == self.member_list[i].email and password == self.member_list[i].password:
                 return 2 # 회원 로그인
-            else
-                return -1
+        
+        return -1 # 회원도 아닌 경우 로그인 실패
         
         
 
-    def insert_Member(Member):
+    def insert_Member(self, Member):
         self.member_list.append(Member)
