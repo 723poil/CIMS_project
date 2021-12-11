2be45f9f2234e60c58212a2fcf07d6aa2311b629
giunsang <33440755+giunsang@users.noreply.github.com>
2021-12-11 14:29:47 +0900

    User_list, Manager_list
    User_list, Manager_list

---
+++ User_package/User_list.py
@@ -0,0 +1,9 @@
+class User_list:
+    def __init__(self, User):
+        self.User = User
+
+    def Check_User_id(email, passord):
+         if User != None and email == User['Email'] and password == User['Password']:
+            return 2 # 회원 로그인
+         else
+            return -1
