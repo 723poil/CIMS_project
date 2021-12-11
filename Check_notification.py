2dbf8eb058459130557c0e9cf2775f949a0534dd
723poil <75459370+723poil@users.noreply.github.com>
2021-12-11 17:52:22 +0900

    알림 리스트에 추가하는 코드 작성

--- Check_notification.py
+++ Check_notification.py
@@ -2,11 +2,12 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-import firebase_application
+import User_package.notification_list
+# import firebase_application
 from firebase_admin import db
 import threading
 
-fa = firebase_application.start()
+# fa = firebase_application.start()
 
 characters = "@."
 
@@ -22,6 +23,10 @@ def thread_run(args):
         if user_noti[i]['isinlist'] == 'no':
             # 알림 창에 추가
             # 추가 후 isinlist 값 변경
+            args[1].noti_insert(user_noti[i])
+            dir = db.reference('User-package/Users/' + db_user_id + '/user_notifications/' + i)
+            user_noti[i]['isinlist'] = 'yes'
+            dir.update(user_noti[i])
             print('개인 알림 추가')
     
     #2. 공통 알림 확인
@@ -32,9 +37,18 @@ def thread_run(args):
     if public_noti['noti_count'] != user_noti_count:
         # 알림 창에 추가
         # 추가 후 user_noti_count 변경
+        while public_noti['noti_count'] != user_noti_count:
+            args[1].noti_insert(public_noti[str(user_noti_count+1)])
+            user_noti_count += 1
+        
+        dir = db.reference('User-package/Users/' + db_user_id)
+        user_noti['inlist_noti'] = user_noti_count
+        dir.update(user_noti)
+
         print('공통 알림 추가')
 
     # 간격 일단 1분마다 설정
     threading.Timer(60, thread_run, args).start()
 
-thread_run('leetkdguq73@naver.com')
+nl = User_package.notification_list.noti()
+thread_run(['leetkdguq73@naver.com', nl])
