29063a4b626a12530c5926bfcca42c6a9be49352
723poil <75459370+723poil@users.noreply.github.com>
2021-12-18 16:16:33 +0900

    ef

--- Covid_package/Corona.py
+++ Covid_package/Corona.py
@@ -3,7 +3,6 @@ class Corona:
     def __init__(self, Corona):
          self.defnition = Corona["definition"]
          self.symptom = Corona["symptom"]
-         self.prophylactic = Corona["prophylactic"] #예방법
          self.how_test = Corona["how_test"]
          self.self_test = Corona["self_test"]
          self.action_tip = Corona["action_tip"]
