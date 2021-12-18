107a5dbd31acf6c5e299b5c851d86ed0d346b5d8
723poil <75459370+723poil@users.noreply.github.com>
2021-12-18 15:01:56 +0900

    gfgf

--- User_package/notification_list.py
+++ User_package/notification_list.py
@@ -7,5 +7,37 @@ class noti:
     def __init__(self):
         self.notification_list = []
 
-    def noti_insert(self, noti_info):
-        self.notification_list.append(noti_info)
+    # 알림 리스트 구성 ( argument -> userid )
+    def set_noti_list(self, userid):
+        
+        characters = "@."
+        
+        db_user_id = ''.join(x for x in userid if x not in characters)
+
+        # 1. 개인 알림 확인
+        user_noti = db.reference('User-package/Users/' + db_user_id + '/user_notifications/').get()
+
+        for i in user_noti:
+            if user_noti[i]['isinlist'] == 'no' and user_noti[i]['read'] == "false":
+                self.notification_list.append(user_noti[i])
+                dir = db.reference('User-package/Users/' + db_user_id + '/user_notifications/' + i)
+                user_noti[i]['isinlist'] = 'yes'
+                dir.update(user_noti[i])
+                print('개인 알림 추가')
+                
+        # 2. 공통 알림 확인
+        public_noti = db.reference('User-package/All-notifications/').get()
+        user_noti = db.reference('User-package/Users/' + db_user_id).get()
+        user_noti_count = user_noti['inlist_noti']
+
+        if public_noti['noti_count'] != user_noti_count:
+            # 추가 후 user_noti_count 변경
+            while public_noti['noti_count'] != user_noti_count:
+                self.notification_list.append(public_noti[str(user_noti_count+1)])
+                user_noti_count += 1
+            
+            dir = db.reference('User-package/Users/' + db_user_id)
+            user_noti['inlist_noti'] = user_noti_count
+            dir.update(user_noti)
+
+            print('공통 알림 추가')
