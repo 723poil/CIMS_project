0fc43580118b35b5bdee6967662e9950bd8bbdcb
723poil <75459370+723poil@users.noreply.github.com>
2021-12-19 14:18:20 +0900

    경로 수정

--- UI/SignIn.py
+++ UI/SignIn.py
@@ -12,18 +12,18 @@ userid = ''
 
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
-SignInWindow = uic.loadUiType("C:/CIMS/UI/SignIn.ui")[0]
-SignUpWindow = uic.loadUiType("C:/CIMS/UI/SignUp.ui")[0]
-UserMenuWindow = uic.loadUiType("C:/CIMS/UI/UserMenu.ui")[0]
-AdminMenuWindow = uic.loadUiType("C:/CIMS/UI/AdminMenu.ui")[0]
-TotalInfoWindow = uic.loadUiType("C:/CIMS/UI/TotalInfoWindow.ui")[0]
-form_reportlist = uic.loadUiType("C:/CIMS/UI/Report_List.ui")[0]
-form_reportcheck = uic.loadUiType("C:/CIMS/UI/Reportcheck.ui")[0]
-form_report = uic.loadUiType("C:/CIMS/UI/Report.ui")[0]
-form_viewinformation = uic.loadUiType("C:/CIMS/UI/ViewInformation.ui")[0]
-form_impactedlist = uic.loadUiType("C:/CIMS/UI/Impacted_List.ui")[0]
-form_visitplacelist = uic.loadUiType("C:/CIMS/UI/VisitPlace_List.ui")[0]
-form_alarmlist = uic.loadUiType("C:/CIMS/UI/Alarm_List.ui")[0]
+SignInWindow = uic.loadUiType("C:/CIMS_PROJECT/UI/SignIn.ui")[0]
+SignUpWindow = uic.loadUiType("C:/CIMS_PROJECT/UI/SignUp.ui")[0]
+UserMenuWindow = uic.loadUiType("C:/CIMS_PROJECT/UI/UserMenu.ui")[0]
+AdminMenuWindow = uic.loadUiType("C:/CIMS_PROJECT/UI/AdminMenu.ui")[0]
+TotalInfoWindow = uic.loadUiType("C:/CIMS_PROJECT/UI/TotalInfoWindow.ui")[0]
+form_reportlist = uic.loadUiType("C:/CIMS_PROJECT/UI/Report_List.ui")[0]
+form_reportcheck = uic.loadUiType("C:/CIMS_PROJECT/UI/Reportcheck.ui")[0]
+form_report = uic.loadUiType("C:/CIMS_PROJECT/UI/Report.ui")[0]
+form_viewinformation = uic.loadUiType("C:/CIMS_PROJECT/UI/ViewInformation.ui")[0]
+form_impactedlist = uic.loadUiType("C:/CIMS_PROJECT/UI/Impacted_List.ui")[0]
+form_visitplacelist = uic.loadUiType("C:/CIMS_PROJECT/UI/VisitPlace_List.ui")[0]
+form_alarmlist = uic.loadUiType("C:/CIMS_PROJECT/UI/Alarm_List.ui")[0]
 
 class TotalInfoClass(QMainWindow, TotalInfoWindow) :
     def __init__(self) :
